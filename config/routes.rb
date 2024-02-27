5a2143060fc23e4219c54e69468a96b2b2837716
Dario Cardoso <darioferreira10@gmail.com>
2024-02-27 12:11:44 -0300

    user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  resource :cars
+  resources :cars
+  resources :users, only: [:show]
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
