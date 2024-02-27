a153da91d90d9f56eb81fa4020dec56538e0e94b
Dario Cardoso <darioferreira10@gmail.com>
2024-02-27 12:48:38 -0300

    rompi todo

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :cars
-  resources :users, only: [:show]
+  # resources :users, only: [:show]
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
