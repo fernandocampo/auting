1e4ff13a82e06ab8519de8e34692c4d1599514bf
antonio edwards <antonio.edwards1992@gmail.com>
2024-02-27 12:15:27 -0300

    saving progress

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,2 +1,33 @@
 class CarsController < ApplicationController
+  def new
+    @car = Car.new
+  end
+
+  def create
+    @car = Car.new(car_params)
+    if @car.save
+      redirect_to @car
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @car = Car.find(params[:id])
+  end
+
+  def update
+    @car = Car.find(params[:id])
+    if @car.update(car_params)
+      redirect_to @car
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def car_params
+    params.require(:car).permit(:brand, :model, :year, :cost_per_day)
+  end
 end
