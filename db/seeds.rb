6628d0acb97ccf3224e3e0171f3bebd36a239446
Dario Cardoso <darioferreira10@gmail.com>
2024-02-28 16:40:55 -0300

    para pull

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,59 +1,49 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
-# fernando = User.create(
-#   email: 'fernando@example.com',
-#   user: 'fernando',
-#   password: '123456',
-#   password_confirmation: '123456',
-#   name: 'Fernando',
-#   last_name: 'Campo',
-#   age: 33,
-#   city: 'CABA',
-#   phone: '12345'
-# )
-
-# antonio = User.create(
-#   email: 'antonio@example.com',
-#   user: 'antonio',
-#   password: 'anto123',
-#   password_confirmation: 'anto123',
-#   name: 'antonio',
-#   last_name: 'edwards',
-#   age: 31,
-#   city: 'Santiago Chile',
-#   phone: '987654321'
-#  )
-
-# dario = User.create(
-#   email: 'dario@example.com',
-#   user: 'dario',
-#   password: 'dario123',
-#   password_confirmation: 'dario123',
-#   name: 'dario',
-#   last_name: 'cardoso',
-#   age: 34,
-#   city: 'CABA',
-#   phone: '111222333'
-# )
-
-# # Crear los autos asociados a un usuario
-# Car.create(brand: 'Honda', model: 'Accord', year: 2020, cost_per_day: 50, user: fernando)
-# Car.create(brand: 'Toyota', model: 'Hilux', year: 2023, cost_per_day: 70, user: antonio)
-# Car.create(brand: 'BMW', model: 'M3', year: 2023, cost_per_day: 250, user: dario)
 require 'faker'
+
+fernando = User.create(
+  email: 'fernando@example.com',
+  user: 'fernando',
+  password: '123456',
+  name: 'Fernando',
+  last_name: 'Campo',
+  age: 33,
+  city: 'CABA',
+  phone: '12345'
+)
+
+antonio = User.create(
+  email: 'antonio@example.com',
+  user: 'antonio',
+  password: 'anto123',
+  name: 'Antonio',
+  last_name: 'Edwards',
+  age: 31,
+  city: 'Santiago Chile',
+  phone: '987654321'
+ )
+
+dario = User.create(
+  email: 'dario@prueba.com',
+  user: 'dario',
+  password: '123456',
+  name: 'Dario',
+  last_name: 'Cardoso',
+  age: 34,
+  city: 'CABA',
+  phone: '111222333'
+)
+
+# Crear los autos asociados a un usuario
+Car.create(brand: 'Honda', model: 'Accord', year: 2020, cost_per_day: 50, user_id: 1)
+Car.create(brand: 'Toyota', model: 'Hilux', year: 2023, cost_per_day: 70, user_id: 2)
+Car.create(brand: 'BMW', model: 'M3', year: 2023, cost_per_day: 250, user_id: 3)
+
 
 puts 'Creating 20 fake users...'
 20.times do
   user = User.create(
     email: Faker::Internet.email,
-    user: Faker::Internet.user,
+    user: Faker::Internet.username,
     password: Faker::Internet.password,
     name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
