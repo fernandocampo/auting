b87464adf1a6855394d6a86ca71f5b85dcfde8e0
Dario Cardoso <darioferreira10@gmail.com>
2024-02-28 15:01:28 -0300

    front end

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'booking/index'
+  get 'booking/show'
+  get 'booking/new'
+  get 'booking/create'
+  get 'booking/edit'
+  get 'booking/update'
+  get 'booking/destroy'
 
   devise_for :users
   resources :cars
