b3dc19473b676072b3540a05719425c988f5a050
Fernando Campo <fernandocampow@gmail.com>
2024-02-28 12:49:58 -0300

    seed
    a

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: '%i(user name last_name age city phone)')
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:user, :name, :last_name, :age, :city, :phone])
   end
 end
