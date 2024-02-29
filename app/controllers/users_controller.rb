1366bac4068bd354c144e9b32e27c9bb7ba6d6d6
Dario Cardoso <darioferreira10@gmail.com>
2024-02-29 09:55:08 -0300

    algunos links

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,11 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @cars = @user.cars
   end
+
+  def my_cars
+    @cars = current_user.cars
+  end
+
 end
