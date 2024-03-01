731efa831a510b0468521cdd23ef66e5c8099b13
antonio edwards <antonio.edwards1992@gmail.com>
2024-03-01 16:13:45 -0300

    texto home

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:user, :name, :last_name, :age, :city, :phone])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:user, :name, :last_name, :age, :city, :phone])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:user, :name, :last_name, :age, :city, :phone, :photo])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:user, :name, :last_name, :age, :city, :phone, :photo])
   end
 end
